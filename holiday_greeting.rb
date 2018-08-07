bf92b35388a59956c17e65002c6680c553db8791
sthai1721 <sophia.thai@rutgers.edu>
2018-08-07 15:18:08 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,5 +1,7 @@
-def holiday_greeting(to, from, event)
+def holiday_greeting(to="Mom", from="Your Favorite Child", event="Mother's Day")
   
-  
+  "Happy #{event}, #{to}! - From #{from}"
   
 end
+
+holiday_greeting
